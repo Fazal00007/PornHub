72b52d2396d4b8809d224280657f8a7c78ddc2e6
Sukuna <111645285+Fazal00007@users.noreply.github.com>
2024-11-10 20:13:51 +0530

    Update __main__.py

--- PornHub/__main__.py
+++ PornHub/__main__.py
@@ -2,7 +2,7 @@ import sys
 import logging
 import pytz
 import platform
-from datetime import datetime, now
+from datetime import datetime
 from importlib import import_module
 
 from PornHub.bot import PornHub
